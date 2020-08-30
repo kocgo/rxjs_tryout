a1b20726c2fe484888dd090b8123a171870df6d4
xheartean <info@gokhankoc.pl>
2020-08-30 12:30:09 +0200

    added dolar sign for observable (convention)

--- src/trackArrayChanges.ts
+++ src/trackArrayChanges.ts
@@ -3,14 +3,14 @@ import { Subject } from "rxjs";
 // Track changes in an Array
 export const trackArrayChanges = () => {
   const myArray = [];
-  const arraySubject: any = new Subject();
+  const arraySubject$: any = new Subject();
 
   const pushToArray = function (item: any) {
     myArray.push(item);
-    arraySubject.next(item);
+    arraySubject$.next(item);
   };
 
-  arraySubject.subscribe(console.log);
+  arraySubject$.subscribe(console.log);
 
   setTimeout(() => pushToArray("hello"), 1000);
   setTimeout(() => pushToArray("hello again"), 2000);
